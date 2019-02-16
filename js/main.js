c38ec8127c6e08d0a9030d9aeb6f12eddfad4ed6
rombrant <45407278+rombrant@users.noreply.github.com>
2019-02-16 13:03:51 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-        //МОБИЛЬНОЕ МЕНЮ, МЕНЮ ГАМБУРГЕР, ГЛАВНОЕ МЕНЮ
+       window.addEventListener('wheeldown', e => {
+            const wrapper = document.querySelector('.wrapper');
+            const sectionHeight = wrapper.firstElementChild.getBoundingClientRect().height;
+            let curHeight = 0;
+            let maxHeight = (wrapper.length - 1) * sectionHeight;
+            wrapper.style.top = sectionHeight+'px';
+       });
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       //МОБИЛЬНОЕ МЕНЮ, МЕНЮ ГАМБУРГЕР, ГЛАВНОЕ МЕНЮ
 
-
+        const methodClose = openMenu();
         function openMenu(){
             const openBtn = document.querySelector('.burgers-menu-button');
             const closeBtn = document.querySelector('.burgers-menu-closed');
@@ -9,6 +28,7 @@
 
 
                 openBtn.addEventListener('click', function () {
+                    removeActiveLink(menuLinks);
                     menu.classList.remove('hide-menu');
                     menu.classList.add('visible');
                     menu.style.opacity= 0;
@@ -35,6 +55,33 @@
                         menu.classList.add('hide-menu');
                     }, 1000);
                 })
+                
+                return  {
+                    close() {
+                        closeBtn.click();
+                    }
+                }
+        }
+        
+        const menuLinks = document.querySelectorAll('.hide__list__item-link');
+        for (const link of menuLinks) {
+            link.addEventListener('click', e=> {
+                const curLink = e.currentTarget;
+                const isClosedLink = curLink.classList.contains("active-link");
+                if (isClosedLink) {
+                    removeActiveLink(menuLinks);
+                } else {
+                    removeActiveLink(menuLinks);
+                    curLink.classList.add('active-link');
+                    setTimeout(methodClose.close(), 1000);
+                }
+            })
+        }
+
+        function removeActiveLink(menuLinks) {
+            Array.from(menuLinks).forEach(link=>{
+                link.classList.remove('active-link');
+            })
         }
         openMenu();
 
